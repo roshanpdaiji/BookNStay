def875e2714c3a56ec833e41b6e54bb5c4794778
roshanpdaiji <roshanpdaijiparekk2002@gmail.com>
2025-11-25 12:49:50 +0530

    webhooks

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -1,7 +1,7 @@
 import User from '../models/User.js'
 import { Webhook } from 'svix'
 
-export const clerkWebhooks = async (req,res) => {
+const clerkWebhooks = async (req,res) => {
     try {
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
 
